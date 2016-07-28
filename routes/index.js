f2e4123d30cbf7f7cb06fed2993651153ac3814e
wmsj100 <wmsj100@hotmail.com>
2016-07-28 08:11:44 +0800

    左侧面板下拉菜单数据获取成功

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-module.exports = function(app){
+module.exports = function(app) {
 
-    app.get('/test', function(req, res){
+    app.get('/test', function(req, res) {
         res.render('tests/index');
     })
 
-    app.get('/', function(req, res){
+    app.get('/', function(req, res) {
         var Mock = require('mockjs'),
             data = null;
         data = {
@@ -27,7 +27,7 @@ module.exports = function(app){
                 url: "#"
             }
         };
-
+        // mock city数据
         data.city = Mock.mock({
             'default': '太原',
             'default_url': '@url("https")',
@@ -37,6 +37,8 @@ module.exports = function(app){
             }],
             'all': '@url("https")'
         });
+
+        // mock 搜索栏的热点词汇
         data.search = Mock.mock({
             'hot|4-8': [{
                 'name': '@cname',
@@ -44,6 +46,69 @@ module.exports = function(app){
                 'boolean': '@boolean(1,3)'
             }]
         });
+
+        // mock 商品分类的具体内容
+        data.drop = Mock.mock({
+            'menu': [{
+                'name': '婚宴酒店',
+                'info': [
+                    {
+                        'name': '星级酒店',
+                        'data|3-8': [{
+                            'name': '@cword(4)',
+                            'url': '@url("http", "huntaotao")'
+                        }]
+                    },
+                    {
+                        'name': '主题酒店',
+                        'data|3-8': [{
+                            'name': '@cword(4)',
+                            'url': '@url("http", "huntaotao")'
+                        }]
+                    }
+                ]
+               
+            }]
+        });
+            // {
+            //     'name': '婚庆服务', 
+            //     'fOne|3-8': '@cword(4)',
+            //     'fTwo|7-20': '@cword(4)',
+            //     'fThree|3-10': '@cword(4)'
+            // },{
+            //     'name': '婚纱摄影', 
+            //     'fOne|3-8': '@cword(4)',
+            //     'fTwo|7-20': '@cword(4)',
+            //     'fThree|3-10': '@cword(4)'
+            // },{
+            //     'name': '婚纱礼服',
+            //     'fOne|3-8': '@cword(4)',
+            //     'fTwo|7-20': '@cword(4)',
+            //     'fThree|3-10': '@cword(4)'
+            // },{
+            //     'name': '婚礼必备/现场布置',
+            //     'fOne|3-8': '@cword(4)',
+            //     'fTwo|7-20': '@cword(4)',
+            //     'fThree|3-10': '@cword(4)'
+            // },{
+            //     'name': '珠宝首饰/品牌商家',
+            //     'fOne|3-8': '@cword(4)',
+            //     'fTwo|7-20': '@cword(4)',
+            //     'fThree|3-10': '@cword(4)'
+            // },{
+            //     'name': '家纺布艺',
+            //     'fOne|3-8': '@cword(4)',
+            //     'fTwo|7-20': '@cword(4)',
+            //     'fThree|3-10': '@cword(4)'
+            // },{
+            //     'name': '礼品糖酒',
+            //     'fOne|3-8': '@cword(4)',
+            //     'fTwo|7-20': '@cword(4)',
+            //     'fThree|3-10': '@cword(4)'
+            // }]
+               
+        // })
         res.render('index', data);
+        // res.send(data);
     });
 }
--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,4 @@ require.config({
     }
 });
 
-require(['app/page/topBar', 'app/page/rightBar', 'app/page/header'], function(){
-
-});
+require(['app/page/topBar', 'app/page/rightBar', 'app/page/header', 'app/page/navBar']);
